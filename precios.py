16df3a030b8095af3bb31700b81790fd29d8d922
nicopez99-unab <n.lpezrivas@uandresbello.edu>
2026-08-30 20:29:43 -0400

    Aplicar cambio solicitado en revisión

--- precios.py
+++ precios.py
@@ -3,16 +3,18 @@ def calcular_total(precio, cantidad):
 
 
 def aplicar_descuento(total, porcentaje):
+    if porcentaje <= 0 or porcentaje > 100:
+        raise ValueError("El porcentaje debe ser mayor a 0 y no mayor a 100")
     descuento = total * porcentaje / 100
     return total - descuento
 
 
-def mostrar_total(precio, cantidad):
+def mostrar_total(precio, cantidad, descuento):
     total = calcular_total(precio, cantidad)
 
-    total = aplicar_descuento(total, 10)
+    total = aplicar_descuento(total, descuento)
 
     print(f"Total con descuento: ${total}")
 
 
-mostrar_total(5000, 3)
+mostrar_total(5000, 3, 10)
